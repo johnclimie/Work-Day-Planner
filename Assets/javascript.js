910e01593ebbe03d2658bca7d2e572b41324c612
John Climie <johnclimie00@gmail.com>
2021-11-30 23:17:04 -0500

    Added README, along with js experimenting

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,1 +1,38 @@
-$("#date").text(moment().format('LL'));
+var now = moment();
+var hour = now.hour();
+
+
+console.log(hour)
+
+$("#date").text(now.format('LL'));
+
+
+$("#test").find('div').find('input').addClass("bg-dark");
+
+
+var test = $("#test").children();
+
+
+
+var test2 = document.getElementById('test');
+
+// for (var i=0; i < test.length; i++) {
+//     $("#test").children('#'+i).addClass("bg-light")
+// }
+
+for (var i = 0; i < test2.children.length; i++) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
